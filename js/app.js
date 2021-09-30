f98cd27a3ab7c294b3d0b7a8d8cda1db35868114
Preston Evans <preston.evans3074@stu.mtec.edu>
2021-09-30 08:29:21 -0600

    meh

--- js/app.js
+++ js/app.js
@@ -1,2 +1,10 @@
-//document.body.style.backgroundColor = "red";
-//test me
+const nav = document.querySelector('nav');
+
+for(let i = 0; i < 7; i++) {
+    nav.innerHTML += `<a href="#">link ${i}</a>`;
+}
+const links = document.querySelectorAll('a');
+
+for(let link of links) {
+    link.classList.add('pretty')
+}
